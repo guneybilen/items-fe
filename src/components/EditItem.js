ad2ef1b5d429e76ee3cc6a151e58da1e3f10912b
guney bilen <guneybilen@yahoo.com>
2022-02-06 21:15:09 -0500

    working on edititem page

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useStoreState, useStoreActions } from 'easy-peasy';
 // import { format } from 'date-fns';
@@ -7,6 +7,15 @@ const EditItem = () => {
   const history = useNavigate();
   const updated = useRef(false);
   const dirty = useRef(false);
+  const scrollRef = useRef(null);
+
+  const [show0, setShow0] = useState(false);
+  const [show1, setShow1] = useState(true);
+  const [show2, setShow2] = useState(true);
+  const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
+  const [show5, setShow5] = useState(false);
+  const [show6, setShow6] = useState(false);
 
   const formEl = useRef(null);
 
@@ -20,6 +29,8 @@ const EditItem = () => {
   const image1 = useStoreState((state) => state.image1);
   const image2 = useStoreState((state) => state.image2);
   const image3 = useStoreState((state) => state.image3);
+  const [error, setError] = useState('');
+  const [closeButtonShouldShow, setCloseButtonShouldShow] = useState(false);
 
   const sluggy = useStoreState((state) => state.slug);
   const setBrand = useStoreActions((actions) => actions.setBrand);
@@ -28,6 +39,9 @@ const EditItem = () => {
   const setImage1 = useStoreActions((actions) => actions.setImage1);
   const setImage2 = useStoreActions((actions) => actions.setImage2);
   const setImage3 = useStoreActions((actions) => actions.setImage3);
+  const [imageUpload1, setImageUpload1] = useState(null);
+  const [imageUpload2, setImageUpload2] = useState(null);
+  const [imageUpload3, setImageUpload3] = useState(null);
 
   const setEntry = useStoreActions((actions) => actions.setEntry);
   const setSlug = useStoreActions((actions) => actions.setSlug);
@@ -35,6 +49,12 @@ const EditItem = () => {
 
   const getItemById = useStoreState((state) => state.getItemById);
   const item = getItemById(slug);
+
+  const scrollTo = (ref) => {
+    if (ref && ref.current /* + other conditions */) {
+      ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
 
   useEffect(() => {
     if (updated.current === true && dirty.current === false) {
@@ -78,9 +98,10 @@ const EditItem = () => {
     console.log(image2);
     console.log(image3);
 
-    if (image1) form_data.append('item_image1', image1);
-    if (image2) form_data.append('item_image2', image2);
-    if (image3) form_data.append('item_image3', image3);
+    if (image1) form_data.append('item_image1', imageUpload1);
+    if (image2) form_data.append('item_image2', imageUpload2);
+    if (image3) form_data.append('item_image3', imageUpload3);
+
     form_data.append('slug', sluggy);
     form_data.append('brand', brand);
     form_data.append('price', price);
@@ -89,12 +110,46 @@ const EditItem = () => {
     form_data.append('seller', localStorage.getItem('seller'));
     form_data.append('nickname', localStorage.getItem('nickname'));
 
-    editItem({ form_data, sluggy });
+    editItem({
+      form_data,
+      sluggy,
+      cb: (brandormodelerror, error, status) => {
+        let error_sentence = null;
+        if (brandormodelerror) {
+          setError(brandormodelerror);
+          setCloseButtonShouldShow(true);
+          scrollTo(scrollRef);
+        } else {
+          error_sentence =
+            status === 400 &&
+            (error?.item_image1 || error?.item_image2 || error?.item_image3);
+          setError(error_sentence);
+          setCloseButtonShouldShow(true);
+          scrollTo(scrollRef);
+        }
+
+        // if (!brandormodelerror && !error_sentence) history('/');
+      },
+    });
+
     updated.current = true;
+  };
+
+  const displayNone = (e) => {
+    e.preventDefault();
+    setCloseButtonShouldShow(false);
   };
 
   return (
     <main className="NewPost">
+      {error && closeButtonShouldShow && (
+        <div className="alert text-center" id="id001" ref={scrollRef}>
+          <span className="closebtn" onClick={(e) => displayNone(e)}>
+            &times;
+          </span>
+          <strong>{error}</strong>
+        </div>
+      )}
       {item && (
         <>
           <h2>Edit Item</h2>
@@ -103,7 +158,6 @@ const EditItem = () => {
             className="newPostForm"
             ref={formEl}
             encType="multipart/form-data"
-            onSubmit={(e) => e.preventDefault()}
           >
             <label htmlFor="itemBrand">Brand:</label>
             <input
@@ -136,38 +190,173 @@ const EditItem = () => {
               value={entry}
               onChange={(e) => setEntry(e.target.value)}
             />
-            <label htmlFor="image1">image1:</label>
-            <input
-              type="file"
-              id="image1"
-              alt="item"
-              name="image"
-              accept="image/*"
-              onChange={(e) => setImage1(e.target.files[0])}
-            />
-            <label htmlFor="image2">image2:</label>
-            <input
-              type="file"
-              id="image2"
-              alt="item"
-              name="image"
-              accept="image/*"
-              onChange={(e) => setImage2(e.target.files[0])}
-            />
-            <label htmlFor="image3">image3:</label>
-            <input
-              type="file"
-              id="image3"
-              alt="item"
-              name="image"
-              accept="image/*"
-              onChange={(e) => setImage3(e.target.files[0])}
-            />
+            <div>
+              <input
+                type="button"
+                style={{ display: show0 ? 'inline-block' : 'none' }}
+                value="undo"
+                id="undobuttonForRemove1"
+                onClick={() => {
+                  setShow1(true);
+                  setShow2(true);
+                  setShow0(false);
+                }}
+              />
+            </div>
+            <div
+              id="forImageButton1"
+              style={{ display: show1 ? 'block' : 'none' }}
+            >
+              <span className="spanImage">
+                <img
+                  src={item?.item_image1}
+                  alt="1"
+                  className={
+                    !!item.item_image1 === false
+                      ? 'itemImageonError'
+                      : 'itemImage'
+                  }
+                  width="150px"
+                  height="75px"
+                />
+              </span>
+
+              <label htmlFor="image1">image1:</label>
+              <input
+                type="file"
+                id="image1"
+                alt="item"
+                name="image"
+                accept="image/*"
+                onChange={(e) => {
+                  e.target.files[0] === undefined
+                    ? setImage1(false)
+                    : setImage1(true);
+                  setImageUpload1(e.target.files[0]);
+                }}
+                onClick={() => {
+                  setShow3(true);
+                  setShow4(true);
+                  setShow2(false);
+                }}
+              />
+              <input
+                type="button"
+                value="remove"
+                id="buttonForRemove1"
+                style={{ display: show2 ? 'inline-block' : 'none' }}
+                onClick={() => {
+                  setShow2(false);
+                  setShow1(false);
+                  setShow0(true);
+                  setImage1(false);
+                }}
+              />
+            </div>
+            <div
+              id="forImageButton2"
+              style={{ display: show3 ? 'block' : 'none' }}
+            >
+              <span className="spanImage">
+                <img
+                  src={item?.item_image2}
+                  alt="2"
+                  className={
+                    !!item.item_image2 === false
+                      ? 'itemImageonError'
+                      : 'itemImage'
+                  }
+                  width="150px"
+                  height="75px"
+                />
+              </span>
+              <label htmlFor="image2">image2:</label>
+              <input
+                type="file"
+                id="image2"
+                alt="item"
+                name="image"
+                accept="image/*"
+                onChange={(e) => {
+                  e.target.files[0] === undefined
+                    ? setImage2(false)
+                    : setImage2(true);
+                  setImageUpload2(e.target.files[0]);
+                }}
+                onClick={() => {
+                  setShow3(true);
+                  setShow4(false);
+                  setShow5(true);
+                  setShow6(true);
+                }}
+              />
+              <input
+                type="button"
+                id="buttonForRemove2"
+                value="Remove"
+                style={{ display: show4 ? 'inline-block' : 'none' }}
+                onClick={() => {
+                  setShow4(false);
+                  setShow3(false);
+                  setShow2(true);
+                  setImage2(false);
+                }}
+              />
+            </div>
+            <div
+              id="forImageButton3"
+              style={{ display: show5 ? 'inline-block' : 'none' }}
+            >
+              <span className="spanImage">
+                <img
+                  src={item.item_image3}
+                  alt="3"
+                  className={
+                    !!item.item_image3 === false
+                      ? 'itemImageonError'
+                      : 'itemImage'
+                  }
+                  width="150px"
+                  height="75px"
+                />
+              </span>
+              <label htmlFor="image3">image3:</label>
+              <input
+                type="file"
+                id="image3"
+                alt="item"
+                name="image"
+                accept="image/*"
+                onChange={(e) => {
+                  e.target.files[0] === undefined
+                    ? setImage3(false)
+                    : setImage3(true);
+                  setImageUpload3(e.target.files[0]);
+                }}
+                onClick={() => {
+                  setShow6(true);
+                }}
+              />
+              <input
+                type="button"
+                style={{ display: show6 ? 'inline-block' : 'none' }}
+                id="buttonForRemove3"
+                value="Remove"
+                onClick={() => {
+                  setShow5(false);
+                  setShow4(true);
+                  setShow6(false);
+                  setImage3(false);
+                }}
+              />
+            </div>
             <button
-              type="button"
-              onClick={() => {
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
                 handleEdit(sluggy);
               }}
+              className="btn btn-primary btn-lg w-100"
             >
               Submit
             </button>
